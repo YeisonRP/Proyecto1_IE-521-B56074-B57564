f1c03809847ec2d0029cbca32ef28e51e7f68567
YeisonRP <yeisonrodriguezpacheco@gmail.com>
2019-06-25 22:29:27 -0600

    Agregando test comentados

--- test/L1cache_test.cpp
+++ test/L1cache_test.cpp
@@ -20,9 +20,14 @@ using namespace std;
 
 
 
-/*
- */
 
+/*
+    1. Escoger una asociatividad random
+    2. Escoger un protocolo random
+    3. Llenar un set de L1\_C1 con asociatividad datos, asegurándose que un dato A está en estado S.
+    4. Llenar un set de L1\_C2 y asegurarse de que el dato A tenga una probabilidad del 50\% de estar o no en este Core (si está es con estado S)
+    5. Forzar un read o write (random 50\%) del dato A y realizar la Verificación 1.
+ */
 TEST(MESI_MSI, ESTADO_S){
 
   int associativityL1, associativityL2;
@@ -37,8 +42,8 @@ TEST(MESI_MSI, ESTADO_S){
 //-- Choose a random address (AddressA)    
   adress_AL1 = rand()%4096; // tag random
 
-  struct entry C1_L1[associativityL1];  // Set de L1
-  struct entry C2_L1[associativityL1];  // Set de L2
+  struct entry C1_L1[associativityL1];  // Set de C1L1
+  struct entry C2_L1[associativityL1];  // Set de C2L1
   struct entry cacheL2[associativityL2];  //Set de L2
   struct operation_result_L2 resultL1L2 = {};
 
@@ -52,19 +57,19 @@ TEST(MESI_MSI, ESTADO_S){
       while(randomTagL1 == adress_AL1);
 
       C1_L1[j].valid = true;
-      C1_L1[j].tag = randomTagL1;                               // Llenando L1
+      C1_L1[j].tag = randomTagL1;                               // Llenando C1L1
       C1_L1[j].dirty = 0;
       C1_L1[j].rp_value = associativityL1-1-j;
 
       C2_L1[j].valid = true;
-      C2_L1[j].tag = randomTagL1;                               // Llenando L1
+      C2_L1[j].tag = randomTagL1;                               // Llenando C2L1
       C2_L1[j].dirty = 0;
       C2_L1[j].rp_value = associativityL1-1-j;
       if(j == 0) // Guarda el valor del tag A en L1 para forzar un hit
       {
         C1_L1[j].tag = adress_AL1;
         C1_L1[j].state = SHARED;
-        if(C2_tiene_A)
+        if(C2_tiene_A)  // Guarda el valor en L1C2
         {
           C2_L1[j].tag = adress_AL1; 
           C2_L1[j].state = SHARED;
@@ -78,9 +83,19 @@ TEST(MESI_MSI, ESTADO_S){
   int hit_C1 = resultL1L2.Hit_L1_C1;
   int hit_C2 = resultL1L2.Hit_L1_C2;
     int CI_C2 = resultL1L2.Coherence_Inv_C2;
+
+  // funcion que hace todo
   lru_L1_L2_replacement_policy(0,adress_AL1,0,adress_AL1,associativityL1,LS,C1_L1,C2_L1,cp,cacheL2,&resultL1L2,false,0);      
 
-////-- CHECKS
+
+/* CHECKS
+    Verificación 1: Si es un read: Verificar que el dato A en C1 se queda en el estado S, y si el dato estaba en C2 revisar 
+                    que también esté en S.
+                    Si es un write: Verificar que el dato A en C1 pasó a M y si el dato A estaba en el C2 que pasó a I.
+    Verificación 2: Verificar que existiera hit segun corresponda y si se pasó al estado I que el C2 regrese
+                    un coherency invalidation.
+ */
+
   if (!LS) // read
   {
     EXPECT_EQ(C1_L1[0].state, SHARED);
@@ -109,6 +124,15 @@ TEST(MESI_MSI, ESTADO_S){
   } 
 }
 
+
+
+/*
+  1. Escoger una asociatividad random
+  2. Llenar un set de L1\_C1 con asociatividad datos, asegurándose que un dato A está en estado M. 
+  3. Llenar un set de L1\_C2 y asegurarse de que el dato A tenga una probabilidad del 50\% 
+     de estar o no en este Core (si está es con estado I)
+  4. Forzar un read o write (random 50\%) del dato A y realizar la Verificación 1.
+ */
 TEST(MESI_MSI, ESTADO_M){
 
   int associativityL1, associativityL2;
@@ -124,34 +148,34 @@ TEST(MESI_MSI, ESTADO_M){
 //-- Choose a random address (AddressA)    
   adress_AL1 = rand()%4096; // tag random
 
-  struct entry C1_L1[associativityL1];  // Set de L1
-  struct entry C2_L1[associativityL1];  // Set de L2
-  struct entry cacheL2[associativityL2];  //Set de L2
+  struct entry C1_L1[associativityL1];      // Set de L1C1
+  struct entry C2_L1[associativityL1];      // Set de L1C2
+  struct entry cacheL2[associativityL2];    //Set de L2
   struct operation_result_L2 resultL1L2 = {};
 
   int randomTagL1;
   bool C2_tiene_A = rand()%2; // 0 no lo tiene, 1 si
 
-//-- LLenar el set de C1 y C2 si correspone, asegurando que el dato A este en C1 y en C2 tal vez, ambos en estado S
+//-- LLenar el set de C1 y C2 si correspone, asegurando que el dato A este en C1 y M, de estar en C2 en estado I
   for (int j =  0; j < associativityL1; j++) {
 
       do{randomTagL1 = rand()%4096; }
       while(randomTagL1 == adress_AL1);
 
       C1_L1[j].valid = true;
-      C1_L1[j].tag = randomTagL1;                               // Llenando L1
+      C1_L1[j].tag = randomTagL1;                               // Llenando L1C1
       C1_L1[j].dirty = 0;
       C1_L1[j].rp_value = associativityL1-1-j;
 
       C2_L1[j].valid = true;
-      C2_L1[j].tag = randomTagL1;                               // Llenando L1
+      C2_L1[j].tag = randomTagL1;                               // Llenando L1C2
       C2_L1[j].dirty = 0;
       C2_L1[j].rp_value = associativityL1-1-j;
-      if(j == 0) // Guarda el valor del tag A en L1 para forzar un hit
+      if(j == 0) // Guarda el valor del tag A en L1C1 para forzar un hit
       {
         C1_L1[j].tag = adress_AL1;
         C1_L1[j].state = MODIFIED;
-        if(C2_tiene_A)
+        if(C2_tiene_A)  // Guarda el valor del tag A en L1C2 
         {
           C2_L1[j].tag = adress_AL1; 
           C2_L1[j].state = INVALID;
@@ -165,9 +189,17 @@ TEST(MESI_MSI, ESTADO_M){
   int hit_C1 = resultL1L2.Hit_L1_C1;
   int CI_C2 = resultL1L2.Coherence_Inv_C2;
   
+  // Funcion que se encarga de todo
   lru_L1_L2_replacement_policy(0,adress_AL1,0,adress_AL1,associativityL1,LS,C1_L1,C2_L1,cp,cacheL2,&resultL1L2,false,0);
 
-////-- CHECKS
+/* CHECKS
+     Verificación 1: Verificar que en cualquier caso el dato se mantiene en M,
+                     y verificar si el dato estaba en C2 que está en invalido y 
+                     que ocurriera un coherency invalidation.
+
+     Verificación 2: que existiera hit segun corresponda y que los datos tengan el 
+                     valor de remplazo adecuad0.
+ */
   if (!LS) // read
   {
     EXPECT_EQ(C1_L1[0].state, MODIFIED);
@@ -187,7 +219,6 @@ TEST(MESI_MSI, ESTADO_M){
     EXPECT_EQ(C1_L1[0].rp_value,0);
     if (C2_tiene_A) // Si el dato esta en C2
     {
-     // EXPECT_EQ(CI_C2 + 1, resultL1L2.Coherence_Inv_C2);
       EXPECT_EQ(C2_L1[0].state, INVALID);
       EXPECT_EQ(C2_L1[0].rp_value,associativityL1-1);
     }
@@ -196,6 +227,13 @@ TEST(MESI_MSI, ESTADO_M){
 
 
 
+
+/*
+    1. Escoger una asociatividad random
+    2. Llenar un set de L1\_C1 con asociatividad datos, asegurándose que un dato A está en estado E. 
+    4. Llenar un set de L1\_C2 y asegurarse de que el dato A no esté.
+    3. Forzar un read o write (random 50\%) del dato A y realizar la Verificación 1.
+ */
 
 TEST(MESI, ESTADO_E){
 
@@ -217,22 +255,22 @@ TEST(MESI, ESTADO_E){
   int randomTagL1;
   bool C2_tiene_A = 0; // 0 no lo tiene, 1 si
 
-//-- LLenar el set de C1 y C2 si correspone, asegurando que el dato A este en C1 y en C2 tal vez, ambos en estado S
+//-- LLenar el set de C1 y C2 si correspone, asegurando que el dato A este en C1 y en C2 tal vez, ambos en estado EXCLUSIVE
   for (int j =  0; j < associativityL1; j++) {
 
       do{randomTagL1 = rand()%4096; }
       while(randomTagL1 == adress_AL1);
 
       C1_L1[j].valid = true;
-      C1_L1[j].tag = randomTagL1;                               // Llenando L1
+      C1_L1[j].tag = randomTagL1;                               // Llenando L1C1
       C1_L1[j].dirty = 0;
       C1_L1[j].rp_value = associativityL1-1-j;
 
       C2_L1[j].valid = true;
-      C2_L1[j].tag = randomTagL1;                               // Llenando L1
+      C2_L1[j].tag = randomTagL1;                               // Llenando L1C2
       C2_L1[j].dirty = 0;
       C2_L1[j].rp_value = associativityL1-1-j;
-      if(j == 0) // Guarda el valor del tag A en L1 para forzar un hit
+      if(j == 0) // Guarda el valor del tag A en L1C1 para forzar un hit
       {
         C1_L1[j].tag = adress_AL1;
         C1_L1[j].state = EXCLUSIVE;
@@ -244,10 +282,19 @@ TEST(MESI, ESTADO_E){
 
   int hit_C1 = resultL1L2.Hit_L1_C1;
   int hit_C2 = resultL1L2.Hit_L1_C2;
+
+  // Funcion que se encarga de todo
   lru_L1_L2_replacement_policy(0,adress_AL1,0,adress_AL1,associativityL1,LS,C1_L1,C2_L1,1,cacheL2,&resultL1L2,false,0);
   
 
-////-- CHECKS
+/* CHECKS
+  Verificación 1: Si es un read:
+                                 Verificar que el dato A en C1 se quede en al estado E.
+                  Si es un write:
+                                 Verificar que el dato A pasa al estado M
+
+  Verificación 2: Verificar que existiera hit segun corresponda y que la política de remplazo del dato A es la adecuada.
+ */
   if (!LS) // read
   {
     EXPECT_EQ(C1_L1[0].state, EXCLUSIVE); 
@@ -255,7 +302,7 @@ TEST(MESI, ESTADO_E){
      EXPECT_EQ(C1_L1[0].rp_value,0);
   }
 
-  if (LS) // store
+  if (LS) // write
   {
     EXPECT_EQ(C1_L1[0].state, MODIFIED); 
     EXPECT_EQ(hit_C1 + 1, resultL1L2.Hit_L1_C1);
@@ -264,7 +311,13 @@ TEST(MESI, ESTADO_E){
 }
 
 
-
+/*
+    1.  Escoger una asociatividad random.
+    2.  Escoger una optimización random.
+    3.  Llenar un bloque de la cache L2 con un dato A asegurándose que esté en estado S
+    4.  Llenar un set de L1 C1 con datos distintos de A y L1 C2 con una probabilidad del 50\% de tener el dato A.
+    5.  Forzar un load o store (50\%) y realizar verificación 1 
+ */
 TEST(L1_L2, miss_hit){
 
   int associativityL1, associativityL2;
@@ -280,30 +333,30 @@ TEST(L1_L2, miss_hit){
 //-- Choose a random address (AddressA)    
   adress_AL1 = rand()%4096; // tag random
 
-  struct entry C1_L1[associativityL1];  // Set de L1
-  struct entry C2_L1[associativityL1];  // Set de L2
+  struct entry C1_L1[associativityL1];  // Set de L1C1
+  struct entry C2_L1[associativityL1];  // Set de L2C2
   struct entry cacheL2[associativityL2];  //Set de L2
   struct operation_result_L2 resultL1L2 = {};
 
   int randomTagL1;
-  bool C2_tiene_A = rand()%2; // 0 no lo tiene, 1 si
+  bool C2_tiene_A = false; // 0 no lo tiene, 1 si
 
-//-- LLenar el set de C1 y C2 si correspone, asegurando que el dato A este en C1 y en C2 tal vez, ambos en estado S
+//-- LLenar el set de C1 y C2 si correspone, asegurando que el dato A no este en C1 y en C2 tal vez
   for (int j =  0; j < associativityL1; j++) {
 
       do{randomTagL1 = rand()%4096; }
       while(randomTagL1 == adress_AL1);
 
       C1_L1[j].valid = true;
-      C1_L1[j].tag = randomTagL1;                               // Llenando L1
+      C1_L1[j].tag = randomTagL1;                               // Llenando L1C1
       C1_L1[j].dirty = 0;
       C1_L1[j].rp_value = associativityL1-1-j;
 
       C2_L1[j].valid = true;
-      C2_L1[j].tag = randomTagL1;                               // Llenando L1
+      C2_L1[j].tag = randomTagL1;                               // Llenando L1C2
       C2_L1[j].dirty = 0;
       C2_L1[j].rp_value = associativityL1-1-j;
-      if(j == 0) // Guarda el valor del tag A en L1 para forzar un hit
+      if(j == 0) // Guarda el valor del tag A en L1C2 para forzar un hit
       {
         if(C2_tiene_A)
         {
@@ -319,11 +372,11 @@ TEST(L1_L2, miss_hit){
       while(randomTagL1 == adress_AL1);
 
       cacheL2[j].valid = true;
-      cacheL2[j].tag = randomTagL1;                               // Llenando L1
+      cacheL2[j].tag = randomTagL1;                               // Llenando L2
       cacheL2[j].dirty = 0;
       cacheL2[j].rp_value = associativityL2-1-j;
 
-      if(j == 0) // Guarda el valor del tag A en L1 para forzar un hit
+      if(j == 0) // Guarda el valor del tag A en L2 para forzar un hit
       {
         cacheL2[j].tag = adress_AL1;
         cacheL2[j].state = SHARED;
@@ -341,7 +394,15 @@ TEST(L1_L2, miss_hit){
   cout << "cp = " << cp << ".   LS= "  << LS << "   DATO EN C2? = " << C2_tiene_A << "   asociatividad = " << associativityL1 << endl;
   lru_L1_L2_replacement_policy(0,adress_AL1,0,adress_AL1,associativityL1,LS,C1_L1,C2_L1,cp,cacheL2,&resultL1L2,false,0);
 
-  ////-- CHECKS
+  /*
+    Checks
+    Verificación 1: Si C2 tiene el dato:  
+                                        LOAD:  Verificar que el dato está en S en C1 y C2
+                                        STORE: Verificar que el dato en C2 pasa a invalido, y que C1 tiene el dato M
+                    Si C2 no tiene el dato:  
+                                        LOAD: Verificar que el dato está en S en C1 
+                                        STORE: Verificar que el dato está en M en C1
+   */
   EXPECT_EQ( resultL1L2.Hit_L2, hit_L2 + 1);
   EXPECT_EQ( resultL1L2.Miss_L1_C1, miss_C1 + 1);
   if (!LS) // read
@@ -383,6 +444,14 @@ TEST(L1_L2, miss_hit){
 }
 
 
+/*
+    1. Escoger una asociatividad random.
+    2. Escoger una optimización random.
+    3. Llenar un bloque de la cache L2 asegurándose que el dato A no esté
+    4. Llenar un set de L1 C1 con datos distintos de A y L1 C2 con una probabilidad del 50\% de tener el dato A en S.
+    5. Forzar un load o store (50\%) y realizar verificación 1 
+ */
+
 
 TEST(L1_L2, miss_miss){
 
@@ -394,32 +463,32 @@ TEST(L1_L2, miss_miss){
   associativityL2 = associativityL1 << 1;
 
 //--    2. Choose a random opt  
-  int cp = rand()%2;
+  int cp = 1;
 
 //-- Choose a random address (AddressA)    
   adress_AL1 = rand()%4096; // tag random
 
-  struct entry C1_L1[associativityL1];  // Set de L1
-  struct entry C2_L1[associativityL1];  // Set de L2
+  struct entry C1_L1[associativityL1];  // Set de L1C1
+  struct entry C2_L1[associativityL1];  // Set de L2C2
   struct entry cacheL2[associativityL2];  //Set de L2
   struct operation_result_L2 resultL1L2 = {};
 
   int randomTagL1;
-  bool C2_tiene_A = rand()%2; // 0 no lo tiene, 1 si
+  bool C2_tiene_A = false; // 0 no lo tiene, 1 si
 
-//-- LLenar el set de C1 y C2 si correspone, asegurando que el dato A este en C1 y en C2 tal vez, ambos en estado S
+//-- LLenar el set de C1 y C2 si correspone, asegurando que el dato no A este en C1 y en C2 tal vez,
   for (int j =  0; j < associativityL1; j++) {
 
       do{randomTagL1 = rand()%4096; }
       while(randomTagL1 == adress_AL1);
 
       C1_L1[j].valid = true;
-      C1_L1[j].tag = randomTagL1;                               // Llenando L1
+      C1_L1[j].tag = randomTagL1;                               // Llenando L1C1
       C1_L1[j].dirty = 0;
       C1_L1[j].rp_value = associativityL1-1-j;
 
       C2_L1[j].valid = true;
-      C2_L1[j].tag = randomTagL1;                               // Llenando L1
+      C2_L1[j].tag = randomTagL1;                               // Llenando L1C2
       C2_L1[j].dirty = 0;
       C2_L1[j].rp_value = associativityL1-1-j;
       if(j == 0) // Guarda el valor del tag A en L1 para forzar un hit
@@ -438,7 +507,7 @@ TEST(L1_L2, miss_miss){
       while(randomTagL1 == adress_AL1);
 
       cacheL2[j].valid = true;
-      cacheL2[j].tag = randomTagL1;                               // Llenando L1
+      cacheL2[j].tag = randomTagL1;                               // Llenando L2
       cacheL2[j].dirty = 0;
       cacheL2[j].rp_value = associativityL2-1-j;
     }  
@@ -452,42 +521,68 @@ TEST(L1_L2, miss_miss){
   int CI_C2 = resultL1L2.Coherence_Inv_C2;
   //debug
   cout << "cp = " << cp << ".   LS= "  << LS << "   DATO EN C2? = " << C2_tiene_A << "   asociatividad = " << associativityL1 << endl;
+  
+  //  funcion que hace todo
   lru_L1_L2_replacement_policy(0,adress_AL1,0,adress_AL1,associativityL1,LS,C1_L1,C2_L1,cp,cacheL2,&resultL1L2,false,0);
 
-  ////-- CHECKS
+
+
+  /*
+    Checks
+    Verificación 1: Si C2 tiene el dato:
+                                        LOAD: Verificar que el dato en C2 se queda en S, también en C1
+                                        STORE: verificar que C2 tenga el dato I y C1 en M
+                    Si C2 no tiene el dato: 
+                                        LOAD: El dato entra en E a C1. 
+                                        STORE: El dato entra a M a C1
+   */
   EXPECT_EQ( resultL1L2.Miss_L2, miss_L2 + 1);
   EXPECT_EQ( resultL1L2.Miss_L1_C1, miss_C1 + 1);
   if (!LS) // read
   {
     for (int i = 0; i < associativityL1; i++)
     {
-      if (C1_L1[i].tag == adress_AL1)
-      {
-        EXPECT_EQ(C1_L1[i].state, SHARED);
-      }
-      if (C2_tiene_A) // Si el dato esta en C2
+      if (C2_tiene_A)  // Si C2 tiene el dato:
       {
         if (C2_L1[i].tag == adress_AL1)
         {
           EXPECT_EQ(C2_L1[i].state, SHARED);
         }
+        if (C1_L1[i].tag == adress_AL1)
+        {
+          EXPECT_EQ(C1_L1[i].state, SHARED);
+        }
+      }
+      else // Si C2 no tiene el dato: 
+      {
+        if (C1_L1[i].tag == adress_AL1)
+        {
+          EXPECT_EQ(C1_L1[i].state, EXCLUSIVE);
+        }
       }    
     }
   }
-  if (LS) // WRITE
+
+  if (LS) // store
   {
     for (int i = 0; i < associativityL1; i++)
     {
-      if (C1_L1[i].tag == adress_AL1)
-      {
-        EXPECT_EQ(C1_L1[i].state, MODIFIED);
-      }
-      if (C2_tiene_A) // Si el dato esta en C2
+      if (C2_tiene_A)  // Si C2 tiene el dato:
       {
         if (C2_L1[i].tag == adress_AL1)
         {
           EXPECT_EQ(C2_L1[i].state, INVALID);
-          EXPECT_EQ(resultL1L2.Coherence_Inv_C2, CI_C2 + 1);
+        }
+        if (C1_L1[i].tag == adress_AL1)
+        {
+          EXPECT_EQ(C1_L1[i].state, MODIFIED);
+        }
+      }
+      else // Si C2 no tiene el dato: 
+      {
+        if (C1_L1[i].tag == adress_AL1)
+        {
+          EXPECT_EQ(C1_L1[i].state, MODIFIED);
         }
       }    
     }
